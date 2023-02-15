63b5e48756ede77c5ef7dcd43a2696ef1950441d
Steven Judd <steven.judd@gmail.com>
2023-02-15 11:55:58 -0600

    updated delay times added location where example code came from

--- 7-Write-Progress.ps1
+++ 7-Write-Progress.ps1
@@ -7,12 +7,14 @@ if ((Get-Host).Name -ne 'ConsoleHost') {
   }
 }
 
+# taken from Example 4 in the help for Write-Progress
+# updated the number of iterations and the delay time
 foreach ( $i in 1..3 ) {
   Write-Progress -Id 0 "Step $i"
   foreach ( $j in 1..5 ) {
     Write-Progress -Id 1 -ParentId 0 "Step $i - Substep $j"
     foreach ( $k in 1..10 ) {
-      Write-Progress -Id 2  -ParentId 1 "Step $i - Substep $j - iteration $k"; Start-Sleep -m 100
+      Write-Progress -Id 2  -ParentId 1 "Step $i - Substep $j - iteration $k"; Start-Sleep -m 50
       # I added the following line
       Write-Host ('.' * $i) '|' ('.' * $j) '|' ('.' * $k)
     }
@@ -24,7 +26,7 @@ $output = @()
 $Level1 = 3
 $Level2 = 5
 $Level3 = 10
-$sleepDuration = 10
+$sleepDuration = 50
 
 foreach ( $i in 1..$Level1 ) {
   Write-Progress -Id 0 "Step $i"
